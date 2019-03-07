e5d5e3ac2e13d35eed2de56ecb2bc1cf41e99419
JiraiyaGerotora <kundankumar18581@gmail.com>
2019-03-07 01:36:14 -0600

    formFeatureset changes

--- DataRecognition.py
+++ DataRecognition.py
@@ -1,20 +1,77 @@
 import re
 import numpy as np
 
+neg_words = ['a','an','the','have','has','been','was','is','by','to','at','for','in','of','from','like','with','were',
+                'are','what','where','how','why','who','it',"it's",'and','but','on',"its",'we','our','over',
+                'under',"about","upon","these","those","this","that","i","they","them","january","february", "march",
+                "april", "may", "june", "july", "august", "september", "october", "november", "december", "monday",
+                "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+
+# previous positive words
+prev_pos_words = ["in", "at", "near", "to", "from", "country of origin"]
+# case independent previous positive words
+# prev_pos_ci_words = ["east", "eastern", "west", "western", "south", "southern", "north", "northern"]
+# post positive words
+post_pos_words = ["City", "County"]
+# case independent post positive words
+post_pos_ci_words = []
+
+# previous negative words
+prev_neg_words = ["University of", "College of"]
+# case independent previous negative words
+prev_neg_ci_words = []
+# post negative words
+post_neg_words = ["University", "Police department"]
+# case independent post negative words
+post_neg_ci_words = []
+
 
 def formFeatureSet(words, entitySet):
     featureFrame = np.array([])
     for index, entity in enumerate(entitySet):
         featureSet = []
 
-        #Check for previous word to be "in" or "at"
-        if  index >= 1 and any(specificWord == words[index-1] for specificWord in ["in", "at", "near"]):
+        # Skip the word in these scenarios:
+        # Place name is always capitalized. If the first character is lower case
+        # if the word is in negative words
+        # if the word ends with 'an'.
+        # TODO - Need to whitelist places ending with 'an'
+        if words[index][0].islower() or words[index].lower() in neg_words or words[index][-2:] == 'an':
+            continue
+
+        #Check for previous positive word
+        if  index > 0 and any(word.split(" ") == words[index-len(word.split(" ")):index] \
+                                                                        for word in prev_pos_words):
             featureSet.append(1)
         else:
             featureSet.append(0)
 
-        #Check for next word exception
-        if index < len(words)-1 and any(specificWord == words[index+1] for specificWord in ["University", "Police department"]):
+        #Check for previous positive word
+#        if  index > 0 and any(word.split(" ") == words[index-len(word.split(" ")):index].lower() \
+#                                                                        for word in prev_pos_ci_words):
+#            featureSet.append(1)
+#        else:
+#            featureSet.append(0)
+
+        # Check for post positive word
+        if index < len(words)-1 and \
+            any(word.split(" ") == words[index+1:index+len(word.split(" "))+1] for word in post_pos_words):
+            featureSet.append(1)
+        else:
+            featureSet.append(0)
+
+        # Check for prev and post positive word - TODO
+
+        #Check for previous negative word exception
+        if index > 0 and \
+            any(word.split(" ") == words[index-len(word.split(" ")):index] for word in post_neg_words):
+            featureSet.append(0)
+        else:
+            featureSet.append(1)
+
+        #Check for post negative word exception
+        if index < len(words)-1 and \
+            any(word.split(" ") == words[index+1:index+len(word.split(" "))+1] for word in post_neg_words):
             featureSet.append(0)
         else:
             featureSet.append(1)
