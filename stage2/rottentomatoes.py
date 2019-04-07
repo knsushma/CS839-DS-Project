166fd924f1ea6532da2b2e6655f08476f15985e4
Sushma K N <sushmakn@Sushmas-MacBook-Pro.local>
2019-04-07 02:50:13 -0500

    CSV file with movie data from RottenTomatoes website

--- stage2/rottentomatoes.py
+++ stage2/rottentomatoes.py
@@ -1,28 +1,49 @@
 from urllib.request import Request, urlopen
+from urllib.error import HTTPError, URLError
 from bs4 import BeautifulSoup
-import json
+import csv
 
-
-extractors = ["Rating", "Genre", "Directed By", "Runtime"]
+# Read links for webpages from which we extract attributes/data
 text_file = open("./rottentomatoes_hyperlinks.txt")
 links = [link.strip("\n") for link in text_file]
-print(links)
 
-for link in links[:10]:
-    web_page = urlopen(Request(link))
+# Final data table that contains multiple tuple with each tuple having attributes (Extractors mentioned below)
+table = []
+table.append(["Name", "Rating", "Genre", "Directed By", "Runtime"])
+extractors = ["Rating", "Genre", "Directed By", "Runtime"]
+
+for link in links:
+    tuple = []
+    web_page = ""
+    # Read contents of the webpage, continue if webpage is Not Found/Unreachable/Operation Timed Out
+    try:
+        web_page = urlopen(Request(link))
+    except HTTPError as e:
+        print("Error Code: ", e)
+        continue
+    except URLError as e:
+        print("Error Code ", e)
+        continue
+
+    #parse the HTML content of the webpage
     soup = BeautifulSoup(web_page, 'html.parser',from_encoding=web_page.info().get_param('charset'))
-
+    #Extract the attributes by tag name
     name = soup.find("h1", {"class" : "mop-ratings-wrap__title mop-ratings-wrap__title--top"}).text
-    print(name)
+    tuple.append(name)
     contents = soup.find_all("li", {"class" : "meta-row clearfix"})
+    attribute_map = {}
     for content in contents:
-         content_soup = BeautifulSoup(str(content), 'html.parser')
-         #print(content)
-         #print(content_soup.find("div", {"class" : "meta-label subtle"}).text.replace(":", "").strip() in extractors)
-         if content_soup.find("div", {"class" : "meta-label subtle"}).text.replace(":", "").strip() in extractors:
-             value = content_soup.find("div", {"class" : "meta-value"}).text
-             print(value.strip())
-    print()
+        content_soup = BeautifulSoup(str(content), 'html.parser')
+        attribute_name = content_soup.find("div", {"class" : "meta-label subtle"}).text.replace(":", "").strip()
+        if  attribute_name in extractors:
+            value = content_soup.find("div", {"class" : "meta-value"}).text
+            attribute_map[attribute_name] = ' '.join(value.split())
+    for e in extractors:
+        tuple.append(attribute_map.get(e, "NA"))
+    table.append(tuple)
+
+csv_file = csv.writer(open("./rottentomatoes_data.csv", "w"), dialect="excel")
+csv_file.writerows(table)
 
 # for link in links[]:
 #
